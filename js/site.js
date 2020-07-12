71f366d6077acc2db08865865fad6663cfce5d6a
Simone Freschi <sfresc01@mail.bbk.ac.uk>
2020-07-12 21:49:50 +0100

    new pages and fixes

--- js/site.js
+++ js/site.js
@@ -1,13 +1,3 @@
-function querystring(obj) {  
-    const result = [];
-    let match;
-    const re = new RegExp('(?:\\?|&)' + obj + '=(.*?)(?=&|$)', 'gi');
-    while ((match = re.exec(document.location.search)) !== null) {
-        result.push(match[1]);
-    }
-    return result;
-}
-
 function showHideHamburger() {
     var x = document.getElementById("hamburgerItems");
     if (x.style.display === "block") {
